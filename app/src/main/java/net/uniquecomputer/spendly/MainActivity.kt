8afca83f651da5a781eeb5030d213237f8c147d0
Sagar Dhadke <peaceunique2003@gmail.com>
2023-07-11 14:06:11 +0530

    Top Menu Added and Fix Navigation Color Background

--- app/src/main/java/net/uniquecomputer/spendly/MainActivity.kt
+++ app/src/main/java/net/uniquecomputer/spendly/MainActivity.kt
@@ -2,6 +2,7 @@ package net.uniquecomputer.spendly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import net.uniquecomputer.spendly.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.Toolbar)
+       supportActionBar?.title = "Records"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
