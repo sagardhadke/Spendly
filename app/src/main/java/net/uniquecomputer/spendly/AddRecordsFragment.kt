2c1de8e8a2c817b47cc205988272056caad66462
Sagar Dhadke <peaceunique2003@gmail.com>
2023-07-13 16:52:51 +0530

    Added DatePicker Dialog in Select Date Option

--- app/src/main/java/net/uniquecomputer/spendly/AddRecordsFragment.kt
+++ app/src/main/java/net/uniquecomputer/spendly/AddRecordsFragment.kt
@@ -3,14 +3,16 @@ package net.uniquecomputer.spendly
 import android.app.DatePickerDialog
 import android.os.Build
 import android.os.Bundle
-import android.provider.SyncStateContract.Constants
 import android.view.LayoutInflater
 import android.view.SurfaceControl.Transaction
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import androidx.annotation.RequiresApi
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import net.uniquecomputer.spendly.databinding.FragmentAddRecordsBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 
 class AddRecordsFragment : BottomSheetDialogFragment() {
@@ -23,7 +25,6 @@ class AddRecordsFragment : BottomSheetDialogFragment() {
     }
 
 
-
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,7 +34,8 @@ class AddRecordsFragment : BottomSheetDialogFragment() {
 
         binding.incomeBtn.setOnClickListener { view ->
             binding.incomeBtn.background = requireContext().getDrawable(R.drawable.income_selector)
-            binding.expenseBtn.background = requireContext().getDrawable(R.drawable.default_selector)
+            binding.expenseBtn.background =
+                requireContext().getDrawable(R.drawable.default_selector)
             binding.expenseBtn.setTextColor(requireContext().getColor(R.color.textColor))
             binding.incomeBtn.setTextColor(requireContext().getColor(R.color.greenColor))
 //            transaction.setType(Constants.INCOME)
@@ -41,7 +43,8 @@ class AddRecordsFragment : BottomSheetDialogFragment() {
 
         binding.expenseBtn.setOnClickListener { view ->
             binding.incomeBtn.background = requireContext().getDrawable(R.drawable.default_selector)
-            binding.expenseBtn.background = requireContext().getDrawable(R.drawable.expense_selector)
+            binding.expenseBtn.background =
+                requireContext().getDrawable(R.drawable.expense_selector)
             binding.incomeBtn.setTextColor(requireContext().getColor(R.color.textColor))
             binding.expenseBtn.setTextColor(requireContext().getColor(R.color.redColor))
 //            transaction.setType(Constants.EXPENSE)
@@ -53,11 +56,25 @@ class AddRecordsFragment : BottomSheetDialogFragment() {
             } else {
                 TODO("VERSION.SDK_INT < N")
             }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                datePickerDialog.setOnDateSetListener { datePicker: DatePicker, i: Int, i1: Int, i2: Int ->
+                    val calendar = Calendar.getInstance()
+                    calendar[Calendar.DAY_OF_MONTH] = datePicker.dayOfMonth
+                    calendar[Calendar.MONTH] = datePicker.month
+                    calendar[Calendar.YEAR] = datePicker.year
+
+                    val SimpleDateFormat = SimpleDateFormat("dd MMMM, yyyy")
+                    val dateToShow: String = SimpleDateFormat.format(calendar.time)
+//                    val dateToShow: String = Helper.formatDate(calendar.time)
+                    binding.date.setText(dateToShow)
+//                    transaction.setDate(calendar.time)
+//                    transaction.setId(calendar.time.time)
+                }
+            }
             datePickerDialog.show()
+
         }
 
         return binding.root
     }
-
-
 }
