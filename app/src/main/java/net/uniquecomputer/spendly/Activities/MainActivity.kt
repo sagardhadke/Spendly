42a7890bd9d2075b34d54fc75c081e60da051985
Sagar Dhadke <peaceunique2003@gmail.com>
2023-07-17 15:36:22 +0530

    Added Current Date in Main Activity

--- app/src/main/java/net/uniquecomputer/spendly/Activities/MainActivity.kt
+++ app/src/main/java/net/uniquecomputer/spendly/Activities/MainActivity.kt
@@ -6,13 +6,19 @@ import android.view.Menu
 import net.uniquecomputer.spendly.Fragments.AddRecordsFragment
 import net.uniquecomputer.spendly.R
 import net.uniquecomputer.spendly.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var calender: Calendar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        calender = Calendar.getInstance()
+
+        updateDate()
 
         setSupportActionBar(binding.Toolbar)
        supportActionBar?.title = "Records"
@@ -22,6 +28,12 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun updateDate() {
+        val SimpleDateFormat = java.text.SimpleDateFormat("dd MMMM yyyy")
+        binding.currentDate.text = SimpleDateFormat.format(calender.time)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.top_menu, menu)
--- app/src/main/java/net/uniquecomputer/spendly/Fragments/AddRecordsFragment.kt
+++ app/src/main/java/net/uniquecomputer/spendly/Fragments/AddRecordsFragment.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
